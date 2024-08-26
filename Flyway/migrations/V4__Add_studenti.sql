d58b906bc6bd63bc7350693fa6f975cb6e4cbfb3
simcal11 <simonecalosc11@gmail.com>
2024-08-26 15:34:05 +0200

    Correzioni minori

--- Flyway/migrations/V4__Add_studenti.sql
+++ Flyway/migrations/V4__Add_studenti.sql
@@ -2,7 +2,7 @@ INSERT INTO studenti(nome, cognome, matricola, email)
 VALUES ('Mario', 'Rossi', 'ARO76', 'mariorossi@email.com');
 
 INSERT INTO studenti(nome, cognome, matricola, email)
-VALUES ('Alice', 'Verdi', 'ALVE45', 'alicerossi@email.com');
+VALUES ('Alice', 'Verdi', 'ALVE45', 'aliceverdi@email.com');
 
 INSERT INTO studenti(nome, cognome, matricola, email)
 VALUES ('Giacomo', 'Bianchi', 'GIBI22', 'giacomobianchi@email.com');
